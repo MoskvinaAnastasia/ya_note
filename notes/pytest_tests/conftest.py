f971aa5800780bab24ad6e27e4c3b358dbc24760
Анастасия Москвина <A.E.Moskvina@yandex.ru>
2024-04-08 20:31:48 +0300

    pytest2

--- notes/pytest_tests/conftest.py
+++ notes/pytest_tests/conftest.py
@@ -9,12 +9,12 @@ from notes.models import Note
 
 @pytest.fixture
 # Используем встроенную фикстуру для модели пользователей django_user_model.
-def author(django_user_model):  
+def author(django_user_model):
     return django_user_model.objects.create(username='Автор')
 
 
 @pytest.fixture
-def not_author(django_user_model):  
+def not_author(django_user_model):
     return django_user_model.objects.create(username='Не автор')
 
 
@@ -41,4 +41,13 @@ def note(author):
         slug='note-slug',
         author=author,
     )
-    return note 
+    return note
+
+
+@pytest.fixture
+# Фикстура запрашивает другую фикстуру создания заметки.
+def slug_for_args(note):
+    # И возвращает кортеж, который содержит slug заметки.
+    # На то, что это кортеж, указывает запятая в конце выражения.
+    return (note.slug,)
+
